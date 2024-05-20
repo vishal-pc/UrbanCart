2cb7dea5d6d13012a8cacbf204867ddf1282706f
Vishal <164140175+vishal-pc@users.noreply.github.com>
2024-05-20 16:42:06 +0530

    Update logger.ts

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -27,12 +27,12 @@ const format = winston.format.combine(
 );
 
 const transports = [
-  new winston.transports.Console(),
-  new winston.transports.File({
-    filename: "logs/error.log",
-    level: "error",
-  }),
-  new winston.transports.File({ filename: "logs/all.log" }),
+  // new winston.transports.Console(),
+  // new winston.transports.File({
+  //   filename: "logs/error.log",
+  //   level: "error",
+  // }),
+  // new winston.transports.File({ filename: "logs/all.log" }),
 ];
 
 const Logger = winston.createLogger({
